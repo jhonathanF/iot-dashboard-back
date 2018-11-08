55e12022e2942d26b061efe40fd0770272ea21f7
jhonathan <jhonathan.luizferreira@gmail.com>
2018-11-08 20:41:53 -0200

    oi

--- src/controllers/distanceController.js
+++ src/controllers/distanceController.js
@@ -1,8 +1,8 @@
+var serialConnection = require('../models/serialConnection')
 
 function getDistance(request, response) {
-    var random = Math.floor(Math.random() * 100);
     response.statusCode = 200;
-    response.write(JSON.stringify({ distance: random }));
+    response.write(JSON.stringify({ distance: serialConnection.getDistance() }));
     response.end();
 }
 
--- src/models/serialConnection.js
+++ src/models/serialConnection.js
@@ -5,26 +5,41 @@ var port;
 async function initSerialConnection() {
     var SerialPort = require('serialport');
 
-    port = new SerialPort('COM3', {
+    port = new SerialPort('COM4', {
         baudRate: 115200
     }, function (err) {
         if (err) {
             return console.log('Error: ', err.message);
         }
     });
-
     var buffer = '';
     port.on('data', function (chunk) {
         buffer += chunk;
-        var answers = buffer.split(/\r?\n/); 
-        buffer = answers.pop(); 
-
+        if (buffer.length > 1000)
+            buffer = '';
+        var regex = /({.+})/
+        var objects = buffer.match(regex);
+        if (objects && objects.length) {
+            var object = objects.pop();
+            buffer = '';
+            try {
+                obj = JSON.parse(object);
+            } catch (e) { }
+            console.log('consegui', obj);
+        }
         console.log(buffer)
-        console.log(answers)
     });
     console.log("Initiated DataBase connection");
 }
+
+var obj = {
+    distance: 0,
+    lightness: 1,
+    temperature: 1
+}
+getDistance = () => obj.distance;
 module.exports = {
     orderBook,
-    initSerialConnection
+    initSerialConnection,
+    getDistance
 }
